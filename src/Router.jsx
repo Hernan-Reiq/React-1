03c24a149d56ebacd9a8d777ea4634816d9fdf27
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-09 17:27:04 -0400

    Ruta si componente y redireccionamiento dentro de la app

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Error from './components/Error';
 import Home from "./components/home";
 import Logos from "./components/logos";
 import StaticComponent from './components/static';
 
 class Router extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <BrowserRouter> {/*Aquí van todas las configuraciones*/}
                 <Switch>{/*Aquí van las rutas*/}
                     <Route exact path="/" component={Home} />
                     <Route exact path="/logos" component={Logos} />
                     <Route exact path="/static" component={StaticComponent} />
+                    <Route exact path="/test" render={() => (
+                        <h1>Esta ruta renderiza un componente dentro de ella, muy mala práctica pero puede ser útil algún día...</h1>
+                    )} />
+                    <Route exact path="/home">
+                        <Redirect to="/" /> {/*ESTO ME REDIRECCIONA A OTRO LUGAR*/}
+                    </Route>
                     <Route component={Error} />
                 </Switch>
             </BrowserRouter>
